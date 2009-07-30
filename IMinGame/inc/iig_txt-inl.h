fcb6653f001b382cba57f8ed6e1f9fc0b1430d54
Eric Sabouraud <esabouraud@users.noreply.github.com>
2009-07-30 00:12:32 +0000

    * missing new source
    git-svn-id: https://svn.code.sf.net/p/imingame/code/trunk@9 accc1846-f7ac-4305-88f2-295ec5a8c1cb

---
+++ IMinGame/inc/iig_txt-inl.h
@@ -0,0 +1,83 @@
+/*
+Copyright (C) 2009, Eric Sabouraud <eric.sabouraud@gmail.com>
+Copyright (C) 2008-2009, Pier Castonguay <dunge2@yahoo.com>
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+- Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+- Neither the name of the Mumble Developers nor the names of its
+  contributors may be used to endorse or promote products derived from this
+  software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+`AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+//IIG_LANGSTR_MACRO(id, eng, fre)
+
+
+IIG_LANGSTR_MACRO(USERMSGLBL,\
+	"Message:",\
+	"Message:")
+
+IIG_LANGSTR_MACRO(USERMSGDEF,\
+	"In-Game",\
+	"Joue à")
+
+IIG_LANGSTR_MACRO(GAMENAMEDEF,\
+	" - Game Title",\
+	" - Titre du jeu")
+
+IIG_LANGSTR_MACRO(REFRESHTIMERLBL,\
+	"Refresh Interval:",\
+	"Période de MAJ")
+
+IIG_LANGSTR_MACRO(ACTMUSICLBL,\
+	"Act as music",\
+	"Emulation musique")
+
+IIG_LANGSTR_MACRO(ACTGAMELBL,\
+	"Act as game (don't show in contact list)",\
+	"Emulation jeu (pas dans la liste de contacts)")
+
+IIG_LANGSTR_MACRO(REFRESHLBL,\
+	"Refresh",\
+	"Mettre à jour")
+
+IIG_LANGSTR_MACRO(MINITRAYLBL,\
+	"Minimize to tray",\
+	"Réduire")
+
+IIG_LANGSTR_MACRO(EXITLBL,\
+	"Exit",\
+	"Quitter")
+
+IIG_LANGSTR_MACRO(EXITMBOX,\
+	"This will stop showing games in your MSN status, are you sure?",\
+	"Votre statut MSN ne sera plus mis à jour avec vos jeux, êtes vous sûr ?")
+
+IIG_LANGSTR_MACRO(HELPTXT,\
+	"The application stays active in the system tray.\nYou can now start a game of your choice and it will appear in MSN Messenger.",\
+	"L'application reste active en arrière-plan.\nVous pouvez maintenant lancer un jeu et il apparaîtra dans MSN Messenger.")
+
+IIG_LANGSTR_MACRO(ENDLANGSTR,\
+	"No language string",\
+	"Pas de texte traduit")
+
